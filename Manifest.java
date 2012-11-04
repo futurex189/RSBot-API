85e99752727a5c5bed30b9feadac6054d1755cbd
futurex189 <futurex189@gmail.com>
2012-11-04 15:05:04 +0800

    Clarify type use

--- Manifest.java
+++ Manifest.java
@@ -2,24 +2,28 @@ package org.powerbot.game.api;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target({TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Manifest {
-	String name();
+    String name();
 
-	String description() default "";
+    String description() default "";
 
-	double version() default 1.0;
+    double version() default 1.0;
 
-	String[] authors();
+    String[] authors();
 
-	String website() default "";
+    String website() default "";
 
-	int topic() default 0;
+    int topic() default 0;
 
-	boolean vip() default false;
+    boolean vip() default false;
 
-	boolean hidden() default false;
+    boolean hidden() default false;
 
-	boolean singleinstance() default false;
+    boolean singleinstance() default false;
 }
